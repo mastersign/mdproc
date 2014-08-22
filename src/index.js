d1acb5d4dd090aff0d4031ae1d47bfafc6b4f194
Tobias Kiertscher <dev@mastersign.de>
2014-08-22 15:51:55 +0200

    implemented extractGraph task

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ var inliner = require('htinliner');
 var ge = require('mdgraphextract');
 var spawn = require('gulp-spawn');
 
+var extractGraph = function(src, dest, opt) {
+	return function() {
+		return src
+			.pipe(ge(opt))
+			.pipe(dest)
+			.pipe(spawn({
+				cmd: 'dot',
+				args: ['-Tsvg', '-Nfontname=Helvetica'],
+				filename: function(base, ext) { return base + '.svg' }
+			}))
+			.pipe(dest);
+	};
+};
+
+module.exports.extractGraphTask = extractGraph;
 
