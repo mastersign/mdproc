f2abe2e66eb7bf49fa3be0a748b314421ef0a996
Tobias Kiertscher <dev@mastersign.de>
2014-09-03 15:30:43 +0200

    added support for docx and pdf

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 var path = require('path');
 var gulp = require('gulp');
 var spawn = require('gulp-spawn');
+var exec = require('gulp-exec');
+var rename = require('gulp-rename');
 
 var ge = require('mdgraphextract');
 var inliner = require('htinliner');
 
 var processStates = require('./states');
+var pdfLang = require('./pdflang');
 
 var inputFormat = [
 	'markdown',
@@ -21,9 +24,17 @@ var inputFormat = [
 	'definition_lists'
 ];
 
-var templatePath = path.join(path.dirname(module.filename), '../assets/template.standalone.html');
+var html5TemplatePath = path.join(path.dirname(module.filename), '../assets/template.standalone.html');
+var latexTemplatePath = path.join(path.dirname(module.filename), '../assets/template.tex');
+var texInputsPath = path.join(path.dirname(module.filename), '../assets');
+
+process.env.TEXINPUTS = texInputsPath + path.delimiter + process.env.TEXINPUTS;
 
 var buildHtml = function(src, dest, opt) {
+	var imgFormat, imgBasePath;
+	opt = opt || {};
+	imgFormat = opt.imgFormat || 'svg';
+	imgBasePath = opt.imgBasePath || dest;
 	return function() {
 		return gulp.src(src)
 			.pipe(processStates())
@@ -32,34 +43,130 @@ var buildHtml = function(src, dest, opt) {
 				args: [
 					'--from=' + inputFormat.join('+'),
 					'--to=html5',
-					'--default-image-extension=svg',
+					'--default-image-extension=' + imgFormat,
 					'--normalize',
 					'--smart',
 					'--toc',
 					'--toc-depth=2',
 					'--mathml',
-					'--template', templatePath
+					'--template="' + html5TemplatePath + '"'
 				],
 				filename: function(base, ext) { return base + '.html' }
 			}))
-			.pipe(inliner({ 
-				svgRemoveSize: true, 
+			.pipe(inliner({
+				svgRemoveSize: true,
 				svgWrapElement: 'div',
-				basePath: dest 
+				basePath: imgBasePath
 			}))
 			.pipe(gulp.dest(dest));
 	};
 };
 module.exports.buildHtmlTask = buildHtml;
 
+
+var buildPdf = function(src, dest, opt) {
+	var execOptions, cmdline;
+	var imgFormat, imgBasePath;
+	opt = opt || {};
+	imgFormat = opt.imgFormat || 'png';
+	imgBasePath = opt.imgBasePath || dest;
+	execOptions = {
+		continueOnError: true,
+		pipeStdout: false
+	};
+	cmdline = [
+		'cd',
+		'"' + imgBasePath + '"',
+		'&&',
+		'pandoc',
+		'--from=' + inputFormat.join('+'),
+		'--to=latex',
+		'--default-image-extension=' + imgFormat,
+		'--normalize',
+		'--smart',
+		'--template="' + latexTemplatePath + '"',
+		'--variable=documentclass:scrartcl',
+		'--variable=lang:<%= file.pdfLang %>',
+		'--latex-engine=xelatex',
+		'-o', '"<%= file.path %>.pdf"',
+		'"<%= file.path %>.tmp"'
+	];
+	return function() {
+		return gulp.src(src)
+			.pipe(processStates())
+			.pipe(rename({ extname: '.tmp' }))
+			.pipe(gulp.dest(dest))
+			.pipe(rename({ extname: '' }))
+			.pipe(pdfLang())
+			.pipe(exec(cmdline.join(' '), execOptions))
+			.pipe(exec.reporter());
+	};
+};
+module.exports.buildPdfTask = buildPdf;
+
+var buildDocx = function(src, dest, opt) {
+	var execOptions, cmdline;
+	var imgFormat, imgBasePath;
+	opt = opt || {};
+	imgFormat = opt.imgFormat || 'png';
+	imgBasePath = opt.imgBasePath || dest;
+	execOptions = {
+		continueOnError: true,
+		pipeStdout: false
+	};
+	cmdline = [
+		'cd',
+		'"' + imgBasePath + '"',
+		'&&',
+		'pandoc',
+		'--from=' + inputFormat.join('+'),
+		'--to=docx',
+		'--default-image-extension=' + imgFormat,
+		'--normalize',
+		'--smart',
+		'-o', '"<%= file.path %>.docx"',
+		'"<%= file.path %>.tmp"'
+	];
+	return function() {
+		return gulp.src(src)
+			.pipe(processStates())
+			.pipe(rename({ extname: '.tmp' }))
+			.pipe(gulp.dest(dest))
+			.pipe(rename({ extname: '' }))
+			.pipe(exec(cmdline.join(' '), execOptions))
+			.pipe(exec.reporter());
+	};
+};
+module.exports.buildDocxTask = buildDocx;
+
 var extractGraph = function(src, dest, opt) {
+	var imgFormat, attributes, a;
+	var args = [];
+	opt = opt || {};
+	imgFormat = opt.imgFormat || 'svg';
+
+	args.push('-T' + imgFormat);
+
+	attributes = opt.graphAttributes || {};
+	for (a in attributes) {
+		args.push('-G' + a + '=' + attributes[a]);
+	}
+	attributes = opt.nodeAttributes || {};
+	for (a in attributes) {
+		args.push('-N' + a + '=' + attributes[a]);
+	}
+	attributes = opt.edgeAttributes || {};
+	for (a in attributes) {
+		args.push('-E' + a + '=' + attributes[a]);
+	}
+
 	return function() {
 		return gulp.src(src)
 			.pipe(ge(opt))
 			.pipe(spawn({
 				cmd: 'dot',
-				args: ['-Tsvg', '-Nfontname=Helvetica'],
-				filename: function(base, ext) { return base + '.svg' }
+				args: args,
+				filename: function(base, ext) { return base + '.' + imgFormat }
 			}))
 			.pipe(gulp.dest(dest));
 	};
---
+++ src/pdflang.js
@@ -0,0 +1,93 @@
+var through = require('through2');
+var yamlheader = require('./yamlheader');
+
+var defLang = 'en';
+var langs = {
+"sq": "albanian",
+"am": "amharic",
+"ar": "arabic",
+"hy": "armenian",
+"eu": "basque",
+"bn": "bengali",
+"br": "breton",
+"bg": "bulgarian",
+"ca": "catalan",
+"hr": "croation",
+"cs": "czech",
+"da": "danish",
+"nl": "dutch",
+"en": "english",
+"eo": "esperanto",
+"et": "estonian",
+"fa": "farsi",
+"fi": "finnish",
+"fr": "french",
+"gl": "galician",
+"de": "german",
+"el": "greek",
+"he": "hebrew",
+"iw": "hebrew",
+"hi": "hindi",
+"is": "icelandic",
+"ia": "interlingua",
+"ga": "irish",
+"it": "italian",
+"kn": "kannada",
+"lo": "lao",
+"la": "latin",
+"lv": "latvian",
+"lt": "lithuanian",
+"ml": "malayalam",
+"mr": "marathi",
+"oc": "occitan",
+"pl": "polish",
+"pt": "portuges",
+"ro": "romanian",
+"ru": "russian",
+"sa": "sanskrit",
+"sr": "serbian",
+"sk": "slovak",
+"sl": "slovenian",
+"es": "spanish",
+"sv": "swedish",
+"ta": "tamil",
+"te": "telugu",
+"bo": "tibetan",
+"th": "thai",
+"tr": "turkish",
+"tk": "turkmen",
+"uk": "ukrainian",
+"ur": "urdu",
+"vi": "vietnamese",
+"cy": "welsh"
+};
+
+var toPdfLang = function(lang) {
+	return langs[lang] || langs[defLang];
+};
+
+var retrieveLang = function(data, opt) {
+	var meta = yamlheader(data, opt);
+	return meta ? meta.lang : undefined;
+};
+
+var pdfLang = function() {
+	return through.obj(function(file, enc, cb) {
+		if (file.isNull()) {
+			this.push(file);
+			cb();
+			return;
+		}
+		if (file.isBuffer()) {
+			file.pdfLang = toPdfLang(retrieveLang(file.contents, { encoding: enc }));
+			this.push(file);
+			cb();
+			return;
+		}
+		if (file.isStream()) {
+			throw 'Streams are not supported.';
+		}
+	});
+};
+
+module.exports = pdfLang;
