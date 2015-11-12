1018c7601ba159a3dd22dc2aa23a910844a1682c
Tobias Kiertscher <dev@mastersign.de>
2015-11-12 13:26:01 +0100

    extended test cases

--- tests/gulpfile.js
+++ tests/gulpfile.js
@@ -13,24 +13,44 @@ gulp.task('csv',
 		'./tmp/',
 		{  }));
 
-gulp.task('dotex', mdproc.extractGraphTask(
-	['./data/architecture.md', './data/backlog.md'], 
-	'./tmp/', 
-	{ mode: 'dotex' }));
+gulp.task('dotex_svg', 
+	mdproc.extractGraphTask(
+		['./data/architecture.md', './data/backlog.md'], 
+		'./tmp/', 
+		{ mode: 'dotex' }));
 
-gulp.task('autograph', mdproc.extractGraphTask(
-	'./data/ui.md', 
-	'./tmp/', 
-	{ autographLevel: 2 }));
+gulp.task('dotex_png', 
+	mdproc.extractGraphTask(
+		['./data/architecture.md', './data/backlog.md'], 
+		'./tmp/', 
+		{ mode: 'dotex', imgFormat: 'png' }));
+
+gulp.task('autograph_svg', 
+	mdproc.extractGraphTask(
+		'./data/ui.md', 
+		'./tmp/', 
+		{ autographLevel: 2 }));
+
+gulp.task('autograph_png', 
+	mdproc.extractGraphTask(
+		'./data/ui.md', 
+		'./tmp/', 
+		{ autographLevel: 2, imgFormat: 'png' }));
 
 var customTextTransform = function (s) { 
 	return s.replace(/included/g, '_included_'); 
 };
 
-gulp.task('md2html', ['dotex', 'autograph'],
+gulp.task('md2html', ['dotex_svg', 'autograph_svg'],
 	mdproc.buildHtmlTask(
 		'./data/*.md',
 		'./tmp/',
 		{ customTransformation: customTextTransform }));
 
-gulp.task('default', ['md2html', 'includes', 'csv']);
+gulp.task('md2docx', ['dotex_png', 'autograph_png'],
+	mdproc.buildDocxTask(
+		'./data/*.md',
+		'./tmp/',
+		{ customTransformation: customTextTransform }));
+
+gulp.task('default', ['includes', 'csv', 'md2html', 'md2docx']);
