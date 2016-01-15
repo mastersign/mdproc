eff965c682e312454b0aa47d948ecbd60aae0b67
Tobias Kiertscher <dev@mastersign.de>
2016-01-15 11:41:55 +0100

    adapted states.js to work with H5smpl 0.2+

--- src/states.js
+++ src/states.js
@@ -1,18 +1,26 @@
 /* global module */
 
+var mapping = {
+	'todo': { style: 'highlight', content: 'ToDo' },
+	'selected': { style: 'highlight', content: 'Selected' },
+	'open': { style: 'active', content: 'Open' },
+	'in-progress': { style: 'info', content: 'In Progress' },
+	'closed': { style: 'success', content: 'Closed' },
+	'invalid': { style: 'warning', content: 'Invalid' },
+	'failed': { style: 'error', content: 'Failed' },
+	'rejected': { style: 'passive', content: 'Rejected' }
+};
+
+var badge = function (typ) {
+	'use strict';
+	return '<div class="badge ' + typ.style + '">' + typ.content + '</div>';
+};
+
 module.exports = function (text) {
+	'use strict';
 	return text.replace(/<!--\s+#state\s+(\S+)\s+-->/g,
-		function (m, typ) {
-			switch (typ) {
-			case 'open':
-				return '<span class="state open">offen</span>';
-			case 'in-progress':
-				return '<span class="state in-progress">in Bearbeitung</span>';
-			case 'closed':
-				return '<span class="state closed">abgeschlossen</span>';
-			default:
-				return '<span class="state unknown">unbekannt</span>';
-			}
+		function (m, state) {
+			return badge(mapping[state] || { style: 'default', content: state });
 		}
 	);
 };
