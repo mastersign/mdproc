cce5e1dcea508d3da32f86d45ef94ff9fc487b97
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2018-09-04 12:29:58 +0000

    handle non existing file in reload module

--- src/reload.js
+++ src/reload.js
@@ -3,6 +3,15 @@
 var through = require('through2');
 var fs = require('fs');
 
+var fileExists = function (path) {
+	try {
+		fs.accessSync(path, fs.constants.F_OK);
+		return true;
+	} catch (err) {
+		return false;
+	}
+};
+
 module.exports = function () {
 	return through.obj(function (file, enc, cb) {
 		if (file.isNull()) {
@@ -13,8 +22,12 @@ module.exports = function () {
 		if (file.isStream()) {
 			file.contents.close();
 		}
-		file.contents = fs.createReadStream(file.path);
-		this.push(file);
+		if (fileExists(file.path)) {
+			file.contents = fs.createReadStream(file.path);
+			this.push(file);
+		} else {
+			console.log("Could not reload file, it does not exist: " + file.path);
+		}
 		cb();
 	});
 };
